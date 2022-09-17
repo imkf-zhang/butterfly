999589ee48f76be91c11ec4158f063df76ee6329
kaifan <15638452918@163.com>
2022-09-17 15:36:08 +0800

    fix: 支持多选花朵

--- relation/index.js
+++ relation/index.js
@@ -1,82 +1,98 @@
 const relation = [
   {
-    title: 'Prunus spinosa',
-    describe: 'Prunus spinosa, called blackthorn or sloe, is a species of flowering plant in the rose family Rosaceae. It attracts pieris napi.',
-    butterfly: ['pieris napi']
-  },
-  {
-    title: 'Brassica nigra',
-    describe:'Brassica nigra, or black mustard, is an annual plant cultivated for its dark-brown-to-black seeds, which are commonly used as a spice. It attracts pieris napi and pieris rapae.',
-    butterfly: ['pieris napi','pieris rapae']
-  },
-  {
     title: 'Ballota nigra',
     describe:'Ballota nigra, black horehound, is a perennial herb of the family Lamiaceae. It attracts pieris napi and pieris rapae.',
-    butterfly: ['pieris napi','pieris rapae']
+    butterfly: ['pieris napi','pieris rapae'],
+    checked: false
   },
   {
     title: 'Picris hieracioides',
     describe:'Picris hieracioides, or hawkweed oxtongue, is a species of flowering plant in the family Asteraceae. It attracts pieris napi and ochlodes venata.',
-    butterfly: ['pieris napi','ochlodes venata']
+    butterfly: ['pieris napi','ochlodes venata'],
+    checked: false
   },
   {
     title: 'Ranunculus sardous',
     describe:'Ranunculus sardous is a species of buttercup known by the common name hairy buttercup. It attracts coenonympha pamphilus.',
-    butterfly: ['coenonympha pamphilus']
+    butterfly: ['coenonympha pamphilus'],
+    checked: false
   },
   {
     title: 'Taraxacum officinale',
     describe:'Taraxacum officinale, the dandelion or common dandelion, is a flowering herbaceous perennial plant of the dandelion genus in the family Asteraceae. It attracts pieris napi and pieris rapae.',
-    butterfly: ['pieris napi','pieris rapae']
+    butterfly: ['pieris napi','pieris rapae'],
+    checked: false
   },
   {
     title: 'Diplotaxis erucoides',
     describe:'Diplotaxis erucoides, the white rocket or white wallrocket, is a species of annual herb of the family Brassicaceae native to West Mediterranean. It attracts pieris napi.',
-    butterfly: ['pieris napi']
+    butterfly: ['pieris napi'],
+    checked: false
   },
   {
     title: 'Rubus ulmifolius',
     describe:'Rubus ulmifolius is a species of wild blackberry known by the English common name elmleaf blackberry or thornless blackberry and the Spanish common name zarzamora. It attracts pieris napi and pieris rapae.',
-    butterfly: ['pieris napi','pieris rapae']
+    butterfly: ['pieris napi','pieris rapae'],
+    checked: false
   },
   {
     title: 'Picris echioides',
     describe:'Picris echioides, known as bristly oxtongue, is a stiff annual or biennial herb native to Europe and North Africa. It attracts pieris napi and pieris rapae.',
-    butterfly: ['pieris napi','pieris rapae']
+    butterfly: ['pieris napi','pieris rapae'],
+    checked: false
   },
   {
     title: 'Trifolium fragiferum',
     describe:'Trifolium fragiferum, the strawberry clover, is a herbaceous perennial plant species in the bean family Fabaceae. It attracts coenonympha pamphilus and plebejus argus.',
-    butterfly: ['coenonympha pamphilus','plebejus argus']
+    butterfly: ['coenonympha pamphilus','plebejus argus'],
+    checked: false
   },
   {
     title: 'Lamium hybridum',
     describe:'Lamium hybridum is an annual and grows primary in the subtropical biomes. It attracts pieris napi.',
-    butterfly: ['pieris napi']
+    butterfly: ['pieris napi'],
+    checked: false
   },
   {
     title: 'Lythrum salicaria',
     describe:'Lythrum salicaria or purple loosestrife is a flowering plant belonging to the family Lythraceae. It attracts pieris rapae and pieris napi.',
-    butterfly: ['pieris napi','pieris rapae']
+    butterfly: ['pieris napi','pieris rapae'],
+    checked: false
   },
   {
     title: 'Trifolium pratense',
     describe:'Trifolium pratense, the red clover, is a herbaceous species of flowering plant in the bean family Fabaceae. It attracts pieris rapae and polyommatus icarus.',
-    butterfly: ['pieris rapae','polyommatus icarus']
+    butterfly: ['pieris rapae','polyommatus icarus'],
+    checked: false
   },
   {
     title: 'Lotus corniculatus',
     describe:"Lotus corniculatus is a flowering plant in the pea family Fabaceae, Common names include common bird's-foot trefoil, eggs and bacon, birdsfoot deervetch, and just bird's-foot trefoil. It attracts plebejus argus and polyommatus icarus.",
-    butterfly: ['plebejus argus','polyommatus icarus']
+    butterfly: ['plebejus argus','polyommatus icarus'],
+    checked: false
   },
   {
     title: 'Sonchus maritimus aquatilis',
     describe:'Sonchus maritimus aquatilis is a species of perennial herb in the family Asteraceae. They have a self-supporting growth form. They have simple, broad leaves and achene. It attracts ochlodes venata.',
-    butterfly: ['ochlodes venata']
+    butterfly: ['ochlodes venata'],
+    checked: false
   },
   {
     title: 'Inula viscosa',
     describe:'Inula viscosa is a well-known medicinal perennial herb, native to the Mediterranean basin. It attracts ochlodes venata.',
-    butterfly: ['ochlodes venata']
+    butterfly: ['ochlodes venata'],
+    checked: false
+  },
+  {
+    title: 'Prunus spinosa',
+    describe: 'Prunus spinosa, called blackthorn or sloe, is a species of flowering plant in the rose family Rosaceae. It attracts pieris napi.',
+    butterfly: ['pieris napi'],
+    checked: false
+  },
+  {
+    title: 'Brassica nigra',
+    describe:'Brassica nigra, or black mustard, is an annual plant cultivated for its dark-brown-to-black seeds, which are commonly used as a spice. It attracts pieris napi and pieris rapae.',
+    butterfly: ['pieris napi','pieris rapae'],
+    checked: false
   }
 ]
